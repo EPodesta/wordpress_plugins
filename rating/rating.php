25b987d3987a93684abc88e13ba5bb347cf0f4dc
Emmanuel Podestá Junior <epodesta158@gmail.com>
2023-02-27 22:02:34 -0300

    Changed again the approach
    This change was due to some problems with metroui and $_POST actions.
    Now this commit introduces the old star html implementation with $_POST.

--- rating/rating.php
+++ rating/rating.php
@@ -34,25 +34,23 @@ function star_rating( $fields ) {
 
     // add new field
     $fields[ 'Rate this post' ] .=
-        '<link rel="stylesheet" href="https://cdn.korzh.com/metroui/v4/css/metro-all.min.css">'.
-        '<div class="rate">
-            <div class="rate_name">
-                Rating
-            </div>
-        </div>'.
-        '<div class="current_post" id="post_id" data-post_id=' . get_the_ID() . '></div>'.
-        '<div class="current_comment" id="comment_id" data-comment_id=' . get_comment_ID() . '></div>
-        <input class="star_rating_field"
-            data-role="rating"
-            id="star_rating_field"
-            data-star-color="orange"
-            data-stared-color="orange"
-            data-on-star-click="
-                <span class="comment_rating"><input type="radio" name="rating" id="rating" value=arguments[0]/>
-            "
-            data-show-score="false"
-        >
-        <script src="https://cdn.korzh.com/metroui/v4/js/metro.min.js"></script>';
+        '<p class="comment-form-rating">'.
+        '<label for="rating">'.__('Rate this post').'<span class="required"> *</span></label>'.
+        '<span class="comment_ratingbox"><br>'.
+        '<div class="stars">'.
+            '<input type="radio" id="star5" name="stars" value="5" />'.
+            '<label for="star5" title="text" class="fas fa-star">5 stars</label>'.
+            '<input type="radio" id="star4" name="stars" value="4" />'.
+            '<label for="star4" title="text" class="fas fa-star">4 stars</label>'.
+            '<input type="radio" id="star3" name="stars" value="3" />'.
+            '<label for="star3" title="text" class="fas fa-star">3 stars</label>'.
+            '<input type="radio" id="star2" name="stars" value="2" />'.
+            '<label for="star2" title="text" class="fas fa-star">2 stars</label>'.
+            '<input type="radio" id="star1" name="stars" value="1" />'.
+            '<label for="star1" title="text" class="fas fa-star">1 star</label>'.
+        '</div><br><br>'.
+        '</span></p>';
+
 
     // add remaining fields
     $fields['cookies'] = $cookies_field;
@@ -60,61 +58,59 @@ function star_rating( $fields ) {
     return $fields;
 }
 
-// Add fields after default fields above the comment box, always visible
-
-add_action( 'comment_form_logged_in_after', 'additional_fields' );
-add_action( 'comment_form_after_fields', 'additional_fields' );
-
-function additional_fields () {
-  echo '<p class="comment-form-title">'.
-  '<label for="title">' . __( 'Comment Title' ) . '</label>'.
-  '<input id="title" name="title" type="text" size="30"  tabindex="5" /></p>';
-
-  echo '<p class="comment-form-rating">'.
-  '<label for="rating">'. __('Rating') . '<span class="required">*</span></label>
-  <span class="commentratingbox">';
-
-    //Current rating scale is 1 to 5. If you want the scale to be 1 to 10, then set the value of $i to 10.
-    for( $i=1; $i <= 5; $i++ )
-    echo '<span class="commentrating"><input type="radio" name="rating" id="rating" value="'. $i .'"/>'. $i .'</span>';
-
-  echo'</span></p>';
-
+function save_comment_meta_data( $comment_id ) {
+    if ((isset($_POST['stars'])))
+        $rating = $_POST['stars'];
+    add_comment_meta($comment_id, 'rating', $rating);
 }
 
-function add_meta_field($post) {
-	add_post_meta($post, 'rating', -1);
-	add_comment_meta($comment, 'rating', -1);
+function insert_comment_rating($comment) {
+    $comment_id = $comment->comment_ID;
+    $comment_rating = get_comment_meta($comment_id,'rating',true);
+    $comment->comment_content .=
+    '<br><input class="comment_rating"
+        id="comment_rating"
+        data-role="rating"
+        data-stared-color="orange"
+        data-static="true"
+        data-value="'.$comment_rating.'"'.
+    '>';
+    return $comment;
 }
 
-function post_rating( $req ) {
-    $rating = $req['rating'];
-    $post_id = $req['post_id'];
-    $comment_id = $req['comment_id'];
-    $res = new WP_REST_Response($comment_id);
-    $res->set_status(200);
-    add_post_meta($post_id, 'rating', $rating, true);
-    return $res;
+function insert_post_rating($post_content) {
+    $post_id = get_the_ID();
+
+    $args = array(
+        'post_id' => $post_id   // Use post_id, not post_ID
+    );
+    $comments = get_comments( $args );
+    $sum_comments = 0.0;
+    $quantity_comments = 0.0;
+    foreach ( $comments as $comment ) {
+        $comment_rate = (float)get_comment_meta($comment->comment_ID,'rating',true);
+        $sum_comments += $comment_rate;
+        $quantity_comments += 1.0;
+    }
+    $mean_comments = $sum_comments/$quantity_comments;
+    return $post_content .
+'<link rel="stylesheet" href="https://cdn.korzh.com/metroui/v4/css/metro-all.min.css">'.
+    '<input class="post_rating"
+        id="post_rating"
+        data-role="rating"
+        data-stared-color="orange"
+        data-static="true"
+        data-value="'.$mean_comments.'"'.
+    '>'.
+'<script src="https://cdn.korzh.com/metroui/v4/js/metro.min.js"></script>';
 }
 
-function show_rating_field($post) {
-    $post_id = $post->ID;
-    $post_rating = get_post_meta($post_id, 'rating', true);
-?>
-<html lang="en">
-    <body>
-        <input class="post_rating"
-            id="post_rating"
-            data-role="rating"
-            data-stared-color="orange"
-            data-static="true"
-            data-value="<?php echo $post_rating?>"
-        >
-    </body>
-</html>
-<?php
+function load_scripts() {
+    wp_enqueue_style('star_style', plugin_dir_url(__FILE__) . '/theme/style.css');
 }
 
+add_action('wp_enqueue_scripts', 'load_scripts');
 add_filter( 'comment_form_fields', 'star_rating');
 add_action( 'comment_post', 'save_comment_meta_data' );
-add_action('the_post', 'show_rating_field');
+add_filter('get_comment', 'insert_comment_rating');
+add_filter('the_content', 'insert_post_rating');
